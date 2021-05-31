3b040f717764c9fad623d87b40ee4797e1893a07
LucasLimaAZ <lucasdelimamonteiro@gmail.com>
2021-05-31 11:59:21 -0300

    environment impovement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,27 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+interface Environment {
+	production: boolean,
+	apiUrl: string,
+	getInventoryDownUrl: string,
+	downloadInventoryDownUrl: string
+}
 
-export const environment = {
-  production: false,
-  apiUrl: "http://localhost:8000/api",
-  getInventoryDownUrl: "http://localhost:8000/api/reports/pdf/get-inventory-down-products",
-  downloadInventoryDownUrl: "http://localhost:8000/api/reports/pdf/download-inventory-down-products"
-};
+export const environment: Environment = {
+	production: false,
+	apiUrl: "https://erpautopecas.herokuapp.com/api",
+	getInventoryDownUrl: "https://erpautopecas.herokuapp.com/api/reports/pdf/get-inventory-down-products",
+	downloadInventoryDownUrl: "https://erpautopecas.herokuapp.com/api/reports/pdf/download-inventory-down-products"
+}
+let hml: boolean
+
+if (hml) {
+	environment.production = false,
+	environment.apiUrl = "http://localhost:8000/api"
+	environment.getInventoryDownUrl = "http://localhost:8000/api/reports/pdf/get-inventory-down-products"
+	environment.downloadInventoryDownUrl = "http://localhost:8000/api/reports/pdf/download-inventory-down-products"
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
